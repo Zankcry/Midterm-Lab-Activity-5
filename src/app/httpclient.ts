a1198a51cdec3bf54115a0aeee072a7f6335141e
Zankcry <101846104+Zankcry@users.noreply.github.com>
2026-02-02 15:54:02 +0800

    Refactor Httpclient methods to implement caching for Genshin characters and users

--- src/app/httpclient.ts
+++ src/app/httpclient.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Httpclient {
+  private usersUrl = 'https://jsonplaceholder.typicode.com/users';
+  private genshinUrl = 'https://genshin.jmp.blue/characters';
   constructor(private http: HttpClient) {}
 
-  getUsersRemotely(): Observable<User[]> {
-    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
+  getGenshinCharacters(): Observable<any> {
+    const cachedData = localStorage.getItem('genshinCharacters');
+    if (cachedData) {
+      return of(JSON.parse(cachedData));
+    }
+    return this.http.get(this.genshinUrl).pipe(
+      tap(data => localStorage.setItem('genshinCharacters', JSON.stringify(data)))
+    );
   }
 
-  getGenshinCharacters(): Observable<any> {
-    return this.http.get('https://genshin.jmp.blue/characters');
+  getUsersRemotely(): Observable<User[]> {
+    const cachedUsers = localStorage.getItem('users');
+    if (cachedUsers) {
+      return of(JSON.parse(cachedUsers));
+    }
+    return this.http.get<User[]>(this.usersUrl).pipe(
+      tap(users => localStorage.setItem('users', JSON.stringify(users)))
+    );
   }
+
+
 }
